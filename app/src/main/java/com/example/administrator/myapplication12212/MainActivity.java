dab3b76f10b2b80032b5ce584a28b0cdd519a415
Administrator <aaa>
2015-03-20 15:48:27 +0800

    增加注释代码

--- app/src/main/java/com/example/administrator/myapplication12212/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication12212/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.myapplication12212;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //测试
+        Log.i("dddddd","ddddddddddddddddddddddd");
+        //d
+        ///df
+        Log.i("dddddd","ddddddddddddddddddddddd");
+
     }
 
 
